94ec990889b514209c635a355ab1629c7ba8afbc
Infinity2209 <anantanag91@gmail.com>
2023-06-19 14:15:37 +0530

    First commit

---
+++ handler.js
@@ -0,0 +1,18 @@
+const message = ({ time, ...rest }) =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`${rest.copy} (with a delay)`);
+    }, time * 1000);
+  });
+
+module.exports.hello = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `Go Serverless v2.0! ${(await message({
+        time: 1,
+        copy: 'Your function executed successfully!',
+      }))}`,
+    }),
+  };
+};
