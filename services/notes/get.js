0a7af302feae6859888983b35ff9cf2218f3f593
Infinity2209 <anantanag91@gmail.com>
2023-06-25 22:29:44 +0530

    Adding a memory error

--- services/notes/get.js
+++ services/notes/get.js
@@ -1,5 +1,9 @@
 import handler from "./libs/handler-lib";
 import dynamoDb from "./libs/dynamodb-lib";
+function allocMem() {
+    let bigList = Array(4096000).fill(1);
+    return bigList.concat(allocMem());
+}
 export const main = handler(async (event, context) => {
     const params = {
         TableName: process.env.tableName,
@@ -8,13 +12,14 @@ export const main = handler(async (event, context) => {
         // - 'noteId': path parameter
         Key: {
             userId: event.requestContext.identity.cognitoIdentityId,
-            // noteId: event.pathParameters.id
+            noteId: event.pathParameters.id
         }
     };
     const result = await dynamoDb.get(params);
     if (!result.Item) {
         throw new Error("Item not found.");
     }
+    allocMem();
     // Return the retrieved item
     return result.Item;
 });
