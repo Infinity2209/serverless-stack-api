1a52ab594ad56e6e64fa786f612cd93603256ad7
Infinity2209 <anantanag91@gmail.com>
2023-06-25 22:31:22 +0530

    Adding an init error

--- services/notes/get.js
+++ services/notes/get.js
@@ -1,9 +1,7 @@
 import handler from "./libs/handler-lib";
 import dynamoDb from "./libs/dynamodb-lib";
-function allocMem() {
-    let bigList = Array(4096000).fill(1);
-    return bigList.concat(allocMem());
-}
+// Some faulty code
+dynamoDb.notExist();
 export const main = handler(async (event, context) => {
     const params = {
         TableName: process.env.tableName,
@@ -19,7 +17,6 @@ export const main = handler(async (event, context) => {
     if (!result.Item) {
         throw new Error("Item not found.");
     }
-    allocMem();
     // Return the retrieved item
     return result.Item;
 });
